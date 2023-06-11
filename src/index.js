b9970b12edf5cba86eaa92ab2faacb61c5a0827e
PakVi <112571887+PakVi@users.noreply.github.com>
2023-06-11 15:09:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 import "./style.css";
 
-document.write("Thou blind fool, Love, what dost thou to mine eyes,\n" +
-    "That they behold, and see not what they see?\n" +
-    "They know what beauty is, see where it lies,\n" +
-    "Yet what the best is take the worst to be.\n" +
-    "If eyes, corrupt by over-partial looks,\n" +
-    "Be anchored in the bay where all men ride,\n" +
-    "Why of eyes’ falsehood hast thou forged hooks,\n" +
-    "Whereto the judgment of my heart is tied?\n" +
-    "Why should my heart think that a several plot,\n" +
-    "Which my heart knows the wide world’s common place?\n" +
-    "Or mine eyes, seeing this, say this is not,\n" +
-    "To put fair truth upon so foul a face?\n" +
-    "In things right true my heart and eyes have erred,\n" +
-    "And to this false plague are they now transferred.");
+document.write("Thou blind fool, Love, what dost thou to mine eyes,\n");
